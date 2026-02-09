b4d721bdc6b7bbd13fb8ba564b0a21ef1a48ce3a
Vando <vandomeri@gmail.com>
2026-02-09 14:31:56 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!-- SFC -->
+<template>
+  <h1 class="heading">ToDo List</h1>
+
+  <div class="todo-item" v-for="todo in todos" :key="todo.id">
+    <span>{{ todo.id }}</span>
+    <span>{{ todo.title }}</span>
+    <span>{{ todo.completed }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { TodoItem } from './types/ToDo'
+
+const todos = ref<TodoItem[]>([
+  { id: 1, title: 'Покормить кота', completed: false },
+  { id: 2, title: 'Позавтракать', completed: true },
+  { id: 3, title: 'Купить молоко', completed: false },
+])
+</script>
+
+<style scoped>
+.heading {
+  color: red;
+  font-size: 50px;
+}
+
+.todo-item {
+  display: flex;
+  align-items: center;
+  column-gap: 40px;
+}
+</style>
---
+++ src/components/CounterItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="decrement()">-</button>
+    <span>{{ counter }}</span>
+    <button @click="increment()">+</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const counter = ref<number>(0)
+
+function increment() {
+  if (counter.value < 10) {
+    counter.value++
+  }
+}
+
+function decrement() {
+  if (counter.value > 0) {
+    counter.value--
+  }
+}
+</script>
