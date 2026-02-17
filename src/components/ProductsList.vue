8431f03b7de5ae39e147214931fb524c66f2e90a
Vando <vandomeri@gmail.com>
2026-02-17 11:45:30 +0300

    added ky

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,6 +1,4 @@
 <template>
-  <h1>Каталог товаров</h1>
-
   <div v-if="products.length" class="grid grid-cols-5 gap-5">
     <div v-for="product in products" :key="product.id">
       <p>{{ product.title }}</p>
@@ -10,26 +8,17 @@
         <span>${{ product.price }}</span>
       </div>
       <p>{{ product.description }}</p>
-      <RouterLink class="px-2 py-1 rounded bg-orange-600 text-white" :to="`/products/${product.id}`"
-        >Подробнее</RouterLink
-      >
     </div>
-  </div>
-  <div v-else>
-    <p>Loading....</p>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Product } from '@/types/Products'
 import { ref } from 'vue'
-import { onMounted } from 'vue'
 
 const products = ref<Product[]>([])
 
-onMounted(async () => {
-  const resp = await fetch('http://localhost:3000/products')
-  const data = await resp.json()
-  products.value = data
-})
+const resp = await fetch('http://localhost:3000/products')
+const data = await resp.json()
+products.value = data
 </script>
--- src/pages/SingleProductPage.vue
+++ src/pages/SingleProductPage.vue
@@ -1,9 +1,26 @@
 <template>
   <h1>Карточка товара</h1>
 
-  {{ $route.params.id }}
-
-  <button>Программно перейти на главную</button>
+  <div v-if="product">
+    <h2>{{ product.title }}</h2>
+    <p>{{ product.description }}</p>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Product } from '@/types/Products'
+import ky from 'ky'
+import { ref } from 'vue'
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const product = ref<Product>()
+
+const route = useRoute()
+
+onMounted(async () => {
+  const data = await ky.get<Product>(`http://localhost:3000/products/${route.params.id}`).json()
+
+  product.value = data
+})
+</script>
